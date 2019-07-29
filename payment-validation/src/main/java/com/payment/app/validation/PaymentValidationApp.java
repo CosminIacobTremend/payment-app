7016b88f514fb050abe8a2cde9b3332bd1d24297
CosminIacobTremend <cosmin.iacob@tremend.com>
2019-07-29 19:02:41 +0300

    Initial structure

---
+++ payment-validation/src/main/java/com/payment/app/validation/PaymentValidationApp.java
@@ -0,0 +1,13 @@
+package com.payment.app.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentValidationApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentValidationApp.class, args);
+  }
+
+}
